b39f4fc2f673017f8dc62b94ff05f9896d92ab6a
yujisoo <sdbwltns@gmail.com>
2022-11-18 07:41:10 +0000

    result page 이름 조회(get) / 이름-텍스트 등록(post)

---
+++ result.js
@@ -0,0 +1,112 @@
+
+
+const axios = require('axios');
+const mysql = require('mysql');
+const express = require("express");
+const app = express();
+const port = 80;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({
+    origin: "http://52.79.253.178",
+    credentials: true,
+  }));
+
+// 이름 받아오기
+app.get('/text/:name', (req, res) => {
+    var name = req.params.name
+    console.log(req.params.name)
+    axios.get('http://15.164.222.12/')
+    .then(function (response) {
+        const connection = mysql.createConnection({
+            multipleStatements: true,
+            host : response.data.host,
+            user : response.data.username,
+            password : response.data.password
+        });
+
+        connection.query("CREATE DATABASE IF NOT EXISTS my_db CHARACTER SET UTF8; USE my_db; CREATE TABLE IF NOT EXISTS result(id INT UNSIGNED NOT NULL AUTO_INCREMENT, name VARCHAR(100) NULL, input_text TEXT NOT NULL, input_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, endpoint TEXT NOT NULL, PRIMARY KEY(id)); SELECT * FROM result WHERE name=?;",[name], function(err, result){
+            if(err){
+                return res.status(404).json({
+                    "error": {
+                        "type": "NOT_FOUND",
+                        "message": "No DB server"
+                    }
+                })
+            }
+            else if(result.length ==  0){
+                return res.status(404).json({
+                    "error": {
+                        "type": "NOT_FOUND",
+                        "message": "No data"
+                    }
+                })
+            }
+            else{
+                return res.status(200).send(result)
+            }
+
+        });
+    })          
+    .catch(function (error) {
+        console.log(error);
+    });
+});
+
+
+
+// 이름 및 텍스트 등록
+app.post('/text', (req, res) => {
+    var name = req.body.name;
+    var text = req.body.text;
+    console.log(name,text)
+    axios.get('http://15.164.222.12/')
+    .then(function (response) {
+        const connection = mysql.createConnection({
+            multipleStatements: true,
+            host : response.data.host,
+            user : response.data.username,
+            password : response.data.password
+        });
+
+        connection.query("CREATE DATABASE IF NOT EXISTS my_db CHARACTER SET UTF8; USE my_db; CREATE TABLE IF NOT EXISTS result(id INT UNSIGNED NOT NULL AUTO_INCREMENT, name VARCHAR(100) NULL, input_text TEXT NOT NULL, input_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, endpoint TEXT NOT NULL, PRIMARY KEY(id)); INSERT INTO result (name, input_text, endpoint) VALUES (?, ?, ?);",[name, text, response.data.host],function(err,result){
+            if(err){
+                console.log(err)
+                res.status(404).json({
+                    "error": {
+                        "type": "NOT_FOUND",
+                        "message": "No DB server"
+                    }
+                })
+            }
+            else{
+                connection.query('SELECT * FROM result WHERE id = (SELECT max(id) FROM result)',function(err2, result){
+                    if(err2){
+                        res.status(404).json({
+                            "error": {
+                                "type": "NOT_FOUND",
+                                "message": "Fail"
+                            }
+                        })
+                    }
+                    else{
+                        res.status(200).send(result)
+    
+                    }
+                });
+            }
+                
+        });
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+});
+
+
+app.listen(port, ()=>console.log(`Server Start. Port : ${port}`));
+
